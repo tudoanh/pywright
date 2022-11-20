211fbf5ec967d25a5d6c3112ff3400bdff865084
tudoanh <tu0703@gmail.com>
2022-11-20 13:13:34 +0700

    move from Flask to FastAPI

--- api_server.py
+++ api_server.py
@@ -1,28 +1,36 @@
-import os
-from flask import Flask, render_template
-from flask import request
-from playwright.sync_api import sync_playwright
+import uvicorn
+from playwright.async_api import async_playwright
 
-app = Flask(__name__)
-pw = sync_playwright().start()
-chrome = pw.chromium.launch(headless=True)
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+
+class URL(BaseModel):
+    url: str
 
 
 # create a view that take POST request contain url param, and return the html
-@app.route("/scrape", methods=["POST"])
-def scrape():
-    url = request.get_json().get("url")
-    page = chrome.new_page()
-    page.goto(url)
-    html = page.content()
-    page.close()
-    return html
+# @app.route("/scrape", methods=["POST"])
+@app.post("/scrape")
+async def scrape(url: URL):
+    async with async_playwright() as pw:
+        browser = await pw.chromium.launch()
+        page = await browser.new_page()
+        await page.goto(url.url)
+        html = await page.content()
+        await browser.close()
+        return HTMLResponse(content=html, status_code=200)
 
 
-@app.route("/", methods=["GET"])
-def index():
-    return render_template("index.html")
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 if __name__ == "__main__":
-    app.run(threaded=False, host="0.0.0.0", port=5000, debug=False)
+    uvicorn.run(app, host="0.0.0.0", port=5000, reload=True)
